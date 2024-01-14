45fa962b1d208c369b5e696b5593e24c486714b9
generalmaks <146928772+generalmaks@users.noreply.github.com>
2024-01-14 19:32:59 +0200

    Step 1,2

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,14 @@
 // Sum all number values in dict
+//Step 1: Change variables types
+//Step 2: Remove unused statements
 
 count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
+  let sum = 0;
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
